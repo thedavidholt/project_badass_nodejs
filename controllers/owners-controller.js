b9adaaf191b3f65d556562a1350c0dfca6a6e5f4
David Holt <thedavidholt@gmail.com>
2023-10-07 09:40:04 -0700

    move data login from controller to model

--- controllers/owners-controller.js
+++ controllers/owners-controller.js
@@ -1,19 +1,8 @@
-const _ = require('lodash')
-const teams = require('../models/teams-model')
+const owner_standings = require('../models/owners-model')
+
+
 
 const owners_index = (req, res) => {
-    const owner_standings = _ .chain(teams)
-    .groupBy('owner')
-    .map((owners_teams,owner) => ({ 
-        owner: owner, 
-        totalPoints: _.sumBy(owners_teams,'points'),
-        teams: owners_teams,})
-    )
-    .orderBy('totalPoints','desc')
-    .value()
-
-    console.log(owner_standings)
-
     res.render('owner-standings', {
     site_title: 'Project Badass', 
     page_name: 'Owner Standings',
---
+++ models/owners-model.js
@@ -0,0 +1,17 @@
+const _ = require('lodash')
+const teams = require('./teams-model')
+
+
+const owner_standings = _ .chain(teams)
+.groupBy('owner')
+.map((owners_teams,owner) => ({ 
+    owner: owner, 
+    totalPoints: _.sumBy(owners_teams,'points'),
+    teams: owners_teams,})
+)
+.orderBy('totalPoints','desc')
+.value()
+
+console.log(owner_standings)
+
+module.exports = owner_standings
